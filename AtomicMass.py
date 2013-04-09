a6c37571ae48b119c7ee6a74b7bac17078f7817a
Stephen Wen <xabupwn@gmail.com>
2013-04-09 19:45:08 -0400

    Fixed the number of first particle error
    The problem was that any future instances of the first element were not
    included when calculating the number of atoms of the first element. This
    was fixed and the program now works as far as I know.

--- AtomicMass.py
+++ AtomicMass.py
@@ -10,13 +10,16 @@ question = input().split(' ')
 elements = question[:-1:2]
 numbers = question[1::2]
 m = float(question[-1])
+numFirst = 0
 MM = 0
 i = 0
 while i<len(elements):
     MM+=am[elements[i]]*int(numbers[i])
+    if elements[i] == elements[0]:
+        numFirst+=int(numbers[i])
     i+=1
 n = m/MM
-particles = n * int(numbers[0]) * 6.022 * 10**23
+particles = n * numFirst * 6.022141 * 10**23
 print('MM = '+ str(MM))
 print('n = '+str(n))
 print('# of first particle = '+str(particles))

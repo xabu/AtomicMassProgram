76c1aa6bfa7f170948bd6671de8dfe113fb69bea
Eric Ye <ericye16@gmail.com>
2013-04-09 20:08:40 -0400

    Added totally unnecessary comments to help myself understand

--- AtomicMass.py
+++ AtomicMass.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python3
+
 perTable = open('periodic_table.txt')
-am = {}
+am = {} # Dictionary going from atomic symbols to masses
 for i in perTable:
-    if i != '':
+    if i:
         line = i.split(',')
-        line[-1] = line[-1][:-1]
-        am[str(line[-1])] = float(line[1])
+        atomicSymbol = line[-1][:-1] #[:-1] to shred off the \n
+        atomicMass = float(line[1])
+        am[atomicSymbol] = atomicMass
 perTable.close()
+print("Sample input: 'H 2 O 1 13.2' for 13.2 grams of H2O")
 question = input().split(' ')
 elements = question[:-1:2]
 numbers = question[1::2]
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ AtomicMassProgram
 A program that takes in a list of elements and the amounts of them and a mass, and spits out the values for Molar Mass, number of moles, and number of atoms of the first element in the formula
 
 I wrote and ran this program in IDLE, the format for the input is Element symbol, followed by a space, followed by the number of that element in the compound, at the end, there will be a space and then the measured mass.
+
+Requires any version of Python 3.

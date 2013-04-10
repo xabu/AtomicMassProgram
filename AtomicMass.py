c2132186b64faf6b9c392351ea66a6ff28b74d55
Eric Ye <ericye16@gmail.com>
2013-04-09 20:31:26 -0400

    Added assuming-1 input

--- AtomicMass.py
+++ AtomicMass.py
@@ -9,21 +9,39 @@ for i in perTable:
         atomicMass = float(line[1])
         am[atomicSymbol] = atomicMass
 perTable.close()
-print("Sample input: 'H 2 O 1 13.2' for 13.2 grams of H2O")
-question = input().split(' ')
-elements = question[:-1:2]
-numbers = question[1::2]
-m = float(question[-1])
-numFirst = 0
-MM = 0
-i = 0
-while i<len(elements):
-    MM+=am[elements[i]]*int(numbers[i])
-    if elements[i] == elements[0]:
-        numFirst+=int(numbers[i])
-    i+=1
-n = m/MM
-particles = n * numFirst * 6.022141 * 10**23
-print('MM = '+ str(MM))
-print('n = '+str(n))
-print('# of first particle = '+str(particles))
+print("Sample input: 'H 2 O 13.2' for 13.2 grams of H2O")
+print("Ctrl + c to stop the program.")
+while(True):
+    question = input().split(' ')
+    # Time to parse the input!
+    elements = []
+    numbers = []
+    m = float(question[-1]) # last thing is _always_ the mass
+    question = question[:-1] #lose the mass
+    i = 0 # we'll do a manual for loop, while-style
+    while i < len(question):
+        element = question[i]
+        #get an element and check if the thing after 1) exists and 2) is a number
+        if i < len(question) - 1 and question[i + 1].isdigit():
+            number = question[i + 1]
+            # if it does, we skip the next thing because it's a number
+            i += 1
+        else:
+            #otherwise assume 1
+            number = 1
+        elements.append(element)
+        numbers.append(number)
+        i += 1
+    numFirst = 0
+    MM = 0
+    i = 0
+    while i<len(elements):
+        MM+=am[elements[i]]*int(numbers[i])
+        if elements[i] == elements[0]:
+            numFirst+=int(numbers[i])
+        i+=1
+    n = m/MM
+    particles = n * numFirst * 6.022141 * 10**23
+    print('MM = '+ str(MM))
+    print('n = '+str(n))
+    print('# of first particle = '+str(particles))
